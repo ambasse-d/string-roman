f79bd4fb965705197a29744157e84212799594bc
ambasse <ambasse.djabiri@akanthas.com>
2022-05-13 14:50:53 +0200

    VALIDATE test for 50

--- app/js/roman-numeral.js
+++ app/js/roman-numeral.js
@@ -4,6 +4,7 @@ function arabicToRoman(number) {
     if (number === 3) return 'III'
     if (number === 5) return 'V'
     if (number === 10) return 'X'
+    if (number === 50) return 'L'
   }
 
 module.exports = { arabicToRoman };
