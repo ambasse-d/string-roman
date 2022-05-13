7272e0574103ffe9e3b58e6ee1e4ac1665c052ea
ambasse <ambasse.djabiri@akanthas.com>
2022-05-13 14:47:08 +0200

    FAILED test for 10

--- app/tests/roman-numeral.test.js
+++ app/tests/roman-numeral.test.js
@@ -5,15 +5,19 @@ test('shoud return I when value equal 1', () => {
   expect(utils.arabicToRoman(1)).toBe('I');
 })
 
-test('shoud return I when value equal 2', () => {
+test('shoud return II when value equal 2', () => {
   expect(utils.arabicToRoman(2)).toBe('II');
 })
 
-test('shoud return I when value equal 3', () => {
+test('shoud return III when value equal 3', () => {
   expect(utils.arabicToRoman(3)).toBe('III');
 })
 
-test('shoud return I when value equal 4', () => {
-  expect(utils.arabicToRoman(4)).toBe('V');
+test('shoud return V when value equal 5', () => {
+  expect(utils.arabicToRoman(5)).toBe('V');
+})
+
+test('shoud return X when value equal 10', () => {
+  expect(utils.arabicToRoman(10)).toBe('X');
 })
 
