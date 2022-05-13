0e0a8f875cd5b1f96a04c1d3ecb9da6ead32aa26
ambasse <ambasse.djabiri@akanthas.com>
2022-05-13 15:49:23 +0200

    change dict for a tab

--- app/js/roman-numeral.js
+++ app/js/roman-numeral.js
@@ -1,22 +1,33 @@
 function arabicToRoman(number) {
-    const dict = {
-      1 : 'I',
-      2 : 'II',
-      3 : 'III',
-      5 : 'V',
-      10 : 'X',
-      50 : 'L',
-      100 : 'C',
-      500 : 'D',
-      1000 : 'M',
-    }
+    const dict = [
+      [1000,'M']
+      [500,'D']
+      [100, 'C']
+      [50 , 'L']
+      [10 , 'X']
+      [5 , 'V']
+      [3 , 'III']
+      [2 , 'II']
+      [1 ,'I']
+    ]
 
     let result = ""
 
-    if (number === 1 || number === 2 || number === 3 ||number === 5 || number === 10 || number === 50 || number === 100 || number === 500 || number === 1000) {
+
+
+    if (number === 1 || 
+        number === 2 || 
+        number === 3 ||
+        number === 5 || 
+        number === 10 || 
+        number === 50 || 
+        number === 100 || 
+        number === 500 || 
+        number === 1000) {
       result +=  dict[number]
     }
-    console.log(result)
+
+    
 
     return result
   }
