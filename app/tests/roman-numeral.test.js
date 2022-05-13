1aaca7259b73345ed5ab9cbaee94d53d9de1d75b
ambasse <ambasse.djabiri@akanthas.com>
2022-05-13 14:36:13 +0200

    FAILED test for 2

--- app/tests/roman-numeral.test.js
+++ app/tests/roman-numeral.test.js
@@ -5,4 +5,7 @@ test('shoud return I when value equal 1', () => {
   expect(utils.arabicToRoman(1)).toBe('I');
 })
 
+test('shoud return I when value equal 2', () => {
+  expect(utils.arabicToRoman(2)).toBe('II');
+})
 
